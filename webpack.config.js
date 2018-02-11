fd80466069dc42fc3552734f4bba90ad5c793376
BoomTech <boomtechny@gmail.com>
2018-02-11 12:02:24 -0500

    Initial setup. Babel and Webpack have been configured.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require("webpack"); 
+var HtmlWebpackPlugin = require('html-webpack-plugin'); 
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html', 
+  filename:'index.html', 
+  inject: 'body'
+}); 
+module.exports = {
+  entry: [
+    './app/index.js'
+  ], 
+  output: {
+    path: __dirname + "/dist", 
+    filename: "index_bundle.js"
+  }, 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  resolve:{
+    modules: [path.resolve(__dirname, 'app'), 'node_modules'
+  ], 
+    extensions: ['*', '.js']
+  }, 
+  plugins: [HtmlWebpackPluginConfig] 
+}
