258489b06e462277c638a859bdaff8b3e3ae7a8f
BoomTech <boomtechny@gmail.com>
2018-02-16 18:18:43 -0500

    Added onSubmit form validation and edited form to utilize redux form. Had to make compatible with redux forms v6.

--- app/components/checkout.js
+++ app/components/checkout.js
@@ -1,77 +1,124 @@
 import React from 'react'; 
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import {reduxForm, Field, validate} from 'redux-form';
 
+
+
+const input= ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div>
+    <label className="control-label">{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} className="form-control" />
+      {touched && ((error && <span className="text-danger">{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+)
 
 class Checkout extends React.Component{
+constructor(){
+  super();
+  this.onSubmit=this.onSubmit.bind(this); 
+}
+
+onSubmit(e){
+  e.preventDefault(); 
+const form = e.target;
+const fields = [form.street,form.floorapt, form.city,form.state, form.postal, form.cardname, form.card,form.exp,form.cvc];
+const isValid = fields.every((f)=>(f.value)); 
+if(isValid){
+  console.log("Form is valid!!!"); 
+
+}
+else{
+  console.log("Form is not valid"); 
+}
+}
+//for(var i = 0; fields.length; i++){
+ // console.log(fields[i].value); 
+//}
+//fields.every(check); 
+//function check(field){
+  //console.log(form.field.value); 
+//}
+
+
+
 
   render(){
+    const {onSubmit} = this.props; 
    // console.log(this.props.selectedProduct);
+   //const { fields: {street, floorapt, city, state, postal, cardname, card, exp, cvc}} = this.props; v5 must migrate to current v6 
     let name = this.props.selectedProduct ? this.props.selectedProduct.name : ''; 
     return <div><h1>Checkout:{name}</h1>
-    <form>
+    <form onSubmit={this.onSubmit}>
       <div className="card">
       <div className="card-header">Address</div>
       <div className="card-body">
       <div className="form-group">
       <label htmlFor="streetAddressInput">Street Address</label>
-<input type = "text" className="form-control" id="addressInput" placeholder="1234 Main St"></input>
+<Field type = "text" className="form-control" id="addressInput" component="input" placeholder="1234 Main St" name ="street" />
       </div>
-      <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
+      <div className="form-group">
+    <label htmlFor="inputAddress2">Address 2</label>
+    <Field type="text" className="form-control" id="inputAddress2" component="input" placeholder="Apartment, studio, or floor" name="floorapt" />
   </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity"></input>
+  <div className="form-row">
+    <div className="form-group col-md-6">
+      <label htmlFor="inputCity">City</label>
+      <Field type="text" className="form-control" id="inputCity" component="input" name="city" />
     </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select class="form-control form-control-large">
-      <option>Select state</option>
-    </select>
+    <div className="form-group col-md-4">
+      <label htmlFor="inputState">State</label>
+      <Field type="text" className="form-control" id="inputState" component="input" name="state"/>
     </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip" placeholder="" required></input>
+    <div className="form-group col-md-2">
+      <label htmlFor="inputZip">Zip</label>
+      <Field type="text" className="form-control" id="inputZip" component="input" placeholder="" name="postal"  />
     </div>
   </div>
   </div>
   </div>
-  <hr class="mb-4"/>
+  <hr className="mb-4"/>
   <div className="card">
   <div className ="card-header">Payment</div>
   <div className = "card-body">
 
-  <div class="form-row">
-  <div class="form-group col-md-6">
-  <label for="cc-name">Name on card</label>
-  <input type="text" class="form-control" id="cc-name" placeholder="" required></input>
+  <div className="form-row">
+  <div className="form-group col-md-6">
+  <label htmlFor="cc-name">Name on card</label>
+  <Field type="text" className="form-control" id="cc-name" placeholder="" component="input" name = "cardname" />
 </div>
-<div class="form-group col-md-6">
-<label for="cc-number">Credit card number</label>
-                <input type="text" class="form-control" id="cc-number" placeholder="" required/>
+<div className="form-group col-md-6">
+<label htmlFor="cc-number">Credit card number</label>
+                <Field type="text" className="form-control" id="cc-number" component="input" placeholder="" name = "card" />
 </div>
-<div class="form-group col-md-3">
-<label for="cc-expiration">Expiration</label>
-                <input type="text" class="form-control" id="cc-expiration" placeholder="NMYY" required/>
+<div className="form-group col-md-3">
+<label htmlFor="cc-expiration">Expiration</label>
+                <Field type="text" className="form-control" id="cc-expiration" component="input" placeholder="NMYY" name = "exp"  />
                 </div>
-                <div class="form-group col-md-3">
-                <label for="cc-expiration">CVV</label>
-                <input type="text" class="form-control" id="cc-cvv" placeholder="CV" required/>
+                <div className="form-group col-md-3">
+                <label htmlFor="cvCode">CVC</label>
+                <Field type="text" className="form-control" id="cc-cvv" component="input" placeholder="CV"  name = "cvc"/>
 </div>
 </div>
 </div>
 </div>
-<hr class="mb-4"/>
-        <button type = "submit" class="btn btn-primary btn-lg btn-block">Next</button>
+<hr className="mb-4"/>
+        <button type = "submit" className="btn btn-primary btn-lg btn-block">Checkout</button>
         
       </form>
       </div>  }
 }
+
+
+
 const mapStateToProps = (state)=>{
   return { selectedProduct: state.selectedProduct}; 
 }
 
 
-export default connect(mapStateToProps)(Checkout); 
+export default reduxForm({
+  form:'Checkout',
+ // validate:(values) => {const {street,floorapt,city,state,postal,cardname,card,exp,cvc} = values; if(!street&&!floorapt&&!city&&!state&&!postal&&!cardname&&!card&&!exp&&!cvc){console.log('Form is not valid');}else{console.log('Form is valid');} return false; }
+ // fields:['street', 'floorapt','city', 'state', 'postal', 'cardname','card','exp','cvc']
+})(connect(mapStateToProps)(Checkout)); 
