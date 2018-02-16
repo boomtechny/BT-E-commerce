bbe8987ed2b234a7ce118e64247a021dd13760c7
BoomTech <boomtechny@gmail.com>
2018-02-16 01:54:12 -0500

    Configured routes utilizing react router v4. Created Checkout component

--- app/components/app.js
+++ app/components/app.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ProductList from './product-list';
+import ProductList from 'components/product-list.js';
+import Checkout from 'components/checkout';
+import {  Route, Switch } from 'react-router-dom'; 
 export default (props) => {
   return <div className="container">
     <div className="page-header">
       <h1>BT Product Catalog</h1>
     </div>
-    <ProductList/>
+ <div className="content">
+
+<Switch>
+   <Route exact path = "/checkout" component = {Checkout}/>
+   <Route exact path = "/" component = {ProductList}/>
+</Switch>
+   </div>
   </div>;
 }
--- app/index.js
+++ app/index.js
@@ -1,14 +1,26 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
-import App from 'components/app.js'
+import App from 'components/app.js';
+
 import reducers from 'reducers'; 
 import {Provider} from 'react-redux'; 
 import { createStore, applyMiddleware } from 'redux'; 
 import ReduxPromise from 'redux-promise'; 
-
+import { HashRouter, Router, Route, Switch } from 'react-router-dom'; 
+import createHashHistory from 'history/createHashHistory'; 
 const store = applyMiddleware(ReduxPromise)(createStore)(reducers);
 
-ReactDOM.render(<Provider store={store}>
-  <App/>
+
+const history = createHashHistory(); 
+ReactDOM.render(
+
+<Provider store={store}>
+<Router history={history}>
+
+<Route path="/" component={App}>
+
+</Route>
+</Router>
+  
   </Provider>, document.getElementById('app')); 
 
