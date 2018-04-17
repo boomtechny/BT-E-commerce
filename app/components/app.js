f7d113b9fe124d2b406c8bada6555b15daca2fda
BoomTech <boomtechny@gmail.com>
2018-04-17 15:12:30 -0400

    Reviewed application added some styling will complete full front-end shop site

--- app/components/app.js
+++ app/components/app.js
@@ -5,12 +5,12 @@ import { Route, Switch } from 'react-router-dom';
 
 
 export default (props) => {
-    let logoSource = require('../images/logo.png'); 
+   /* let logoSource = require('https://s3.amazonaws.com/boomtechny/web/logo.png'); */
 
     return <div className = "body_container"> 
     <div className = "header_wrapper">
         <div className = "header">
-        <img className = "header_logo" src={logoSource}/>
+        <img className = "header_logo" src="https://s3.amazonaws.com/boomtechny/web/logo.png"/>
 </div>
 </div>
 <div className = "container">
