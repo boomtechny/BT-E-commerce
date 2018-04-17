cec9b9447dc084d6f60262cefaa51329d54dd18a
BoomTech <boomtechny@gmail.com>
2018-04-17 14:51:14 -0400

    Reviewed application added some styling will complete full front-end shop site

--- app/components/app.js
+++ app/components/app.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ProductList from 'components/product-list.js';
 import Checkout from 'components/checkout';
-import {  Route, Switch } from 'react-router-dom'; 
+import { Route, Switch } from 'react-router-dom';
 
 
 export default (props) => {
-  return <div className="container">
-    <div className="page-header">
-      <h1>BT Product Catalog</h1>
-    </div>
- <div className="content">
+    let logoSource = require('../images/logo.png'); 
 
-<Switch>
-   <Route exact path = "/checkout" component = {Checkout}/>
-   <Route exact path = "/" component = {ProductList}/>
-</Switch>
-   </div>
-  </div>;
+    return <div className = "body_container"> 
+    <div className = "header_wrapper">
+        <div className = "header">
+        <img className = "header_logo" src={logoSource}/>
+</div>
+</div>
+<div className = "container">
+        <div className = "content" >
+
+        <Switch >
+        <Route exact path = "/checkout" component = { Checkout }/> <Route exact path = "/" component ={ ProductList }
+    /> </Switch> </div></div></div>;
 }
